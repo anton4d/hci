1db13b35b761026ec9b8bb0bd76ac67efefe8386
anton <antonlukas25@gmail.com>
2021-11-30 09:18:12 +0100

    html add

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+    fs.readFile('demofile1.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+      });
 });
 
 server.listen(port, hostname, () => {
